38a5424eaee2c886ad6644b82295ebe9d0c0674a
Ylana Portela <ylanaportela2000@gmail.com>
2022-02-20 00:12:57 -0300

    Add body of app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+
+  const tips = [5,10,15,25,50, 'Custom']
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+
+      <div className="container-title">
+        <h1>SPLITTER</h1>
+      </div>
+
+      <div className="container-calculator">
+
+        <div className="container-calculator__input">
+          <div className="bill">
+            <p>Bill</p>
+              <input className="bill-input"></input>
+          </div>
+
+          <div className="tip">
+            <p>Select tip %</p>
+            <div className="tip-buttons">
+              {tips.forEach(tip =>{
+                return (
+                  <div>
+                    <div>{tip}%</div>
+                  </div>
+                )
+              })}
+            </div>
+          </div>
+
+          <div className="people">
+            <p>Number of People</p>
+            <input className="bill-input"></input>
+          </div>
+
+        </div>
+        <div className="container-calculator__output"></div>
+      </div>
     </div>
   );
 }
