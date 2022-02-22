28ebec0eaa8ff45d2a878df6b18b6aafd83c481e
Ylana Portela <ylanaportela2000@gmail.com>
2022-02-21 21:35:26 -0300

    Add new features

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import logo from './images/logo.svg'
+import { useState } from 'react';
+
+import './App.scss'
 function App() {
 
-  const tips = [5,10,15,25,50, 'Custom']
+  const tips = [5, 10, 15, 25, 50, 'Custom']
 
   return (
     <div className="container">
 
       <div className="container-title">
-        <h1>SPLITTER</h1>
+        <img src={logo} alt='logo' />
       </div>
 
       <div className="container-calculator">
@@ -14,30 +18,55 @@ function App() {
         <div className="container-calculator__input">
           <div className="bill">
             <p>Bill</p>
-              <input className="bill-input"></input>
+            <input className="bill-input" />
           </div>
 
           <div className="tip">
             <p>Select tip %</p>
             <div className="tip-buttons">
-              {tips.forEach(tip =>{
+              {tips.map(tip => {
                 return (
-                  <div>
-                    <div>{tip}%</div>
-                  </div>
-                )
+                  <div key={tip}>{tip}</div>
+            )
               })}
-            </div>
           </div>
-
-          <div className="people">
-            <p>Number of People</p>
-            <input className="bill-input"></input>
-          </div>
-
         </div>
-        <div className="container-calculator__output"></div>
+
+        <div className="people">
+          <p>Number of People</p>
+          <input className="bill-input" />
+        </div>
+
       </div>
+
+      <div className="container-calculator__output">
+
+        <div className='output-values'>
+          <div className='output-values__tip'>
+
+            <div className='text'>
+              <div className='text-title'>Tip Amount</div>
+              <p className='text-description'> /person</p>
+            </div>
+
+            <div className='value'><span></span></div>
+          </div>
+
+          <div className='output-values__total'>
+
+            <div className='text'>
+              <div className='text-title'>Total</div>
+              <p className='text-description'> /person</p>
+            </div>
+
+            <div className='value'><span>$</span></div>
+          </div>
+
+          <button>RESET</button>
+        </div>
+
+      </div>
+    </div>
     </div>
   );
 }
